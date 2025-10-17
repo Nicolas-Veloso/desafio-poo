46834c47255e5d7b0b4293b3c1578c20a3121589
Nicolas <nicolasrodriguesveloso@gmail.com>
2025-10-17 15:46:32 -0300

    refac: removi imports desnecessarios

--- src/service/InquilinoService.java
+++ src/service/InquilinoService.java
@@ -1,14 +1,11 @@
 package service;
 
-import domain.Imovel;
 import domain.Inquilino;
-import domain.Proprietario;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class InquilinoService {
-    ImovelService imovelService = new ImovelService();
 
     List<Inquilino> listaDeInquilinos = new ArrayList<>();
 
